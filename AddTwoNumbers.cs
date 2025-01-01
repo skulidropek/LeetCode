5f41af76ed4c19a2e7083fe7e0964b94483388f6
skulidropek <66840575+skulidropek@users.noreply.github.com>
2025-01-01 21:26:13 +0400

    https://leetcode.com/problems/add-two-numbers/submissions/1494289720/

---
+++ AddTwoNumbers.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Numerics;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class AddTwoNumbersTast
+    {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var value = GetNumber(l1) + GetNumber(l2);
+
+            var result = CreateListNode(value);
+
+            return result;
+        }
+
+        public static ListNode CreateListNode(BigInteger number)
+        {
+            var remainder = (int)(number % 10);
+            ListNode result = new ListNode(remainder);
+
+            number /= 10;
+
+            if(number == 0)
+                return result;
+
+            result.next = CreateListNode(number);
+            return result;
+        }
+
+        public static BigInteger GetNumber(ListNode listNode)
+        {
+            if (listNode.next == null)
+                return listNode.val;
+
+            var next = GetNumber(listNode.next) * 10;
+            return listNode.val + next;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeetCode;
+using Newtonsoft.Json;
+using System.Numerics;
+using System.Text.Json.Serialization;
+
+//ListNode l1 = new ListNode(1,
+//    new ListNode(0,
+//        new ListNode(0,
+//            new ListNode(0,
+//                new ListNode(0,
+//                    new ListNode(0,
+//                        new ListNode(0,
+//                            new ListNode(0,
+//                                new ListNode(0,
+//                                    new ListNode(0,
+//                                        new ListNode(0,
+//                                            new ListNode(0,
+//                                                new ListNode(0,
+//                                                    new ListNode(0,
+//                                                        new ListNode(0,
+//                                                            new ListNode(0,
+//                                                                new ListNode(0,
+//                                                                    new ListNode(0,
+//                                                                        new ListNode(0,
+//                                                                            new ListNode(1)
+//                                                                        )
+//                                                                    )
+//                                                                )
+//                                                            )
+//                                                        )
+//                                                    )
+//                                                )
+//                                            )
+//                                        )
+//                                    )
+//                                )
+//                            )
+//                        )
+//                    )
+//                )
+//            )
+//        )
+//    )
+//);
+
+ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+var result = AddTwoNumbersTast.AddTwoNumbers(
+    l1,
+    l2
+);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+
+Console.WriteLine(result.GetNumber());
+//Console.WriteLine(BigInteger.Parse(result.ToStringExtension()) + " " + result.ToString());
