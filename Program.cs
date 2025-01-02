9eef4b67bb142cfbac4c4b0d2132dca516f25b3b
skulidropek <66840575+skulidropek@users.noreply.github.com>
2025-01-02 22:17:15 +0400

    Сделал алгоритм который выполняется за 29млс
    https://leetcode.com/problems/two-sum/submissions/1495414254/

--- Program.cs
+++ Program.cs
@@ -3,4 +3,6 @@ using Newtonsoft.Json;
 using System.Numerics;
 using System.Text.Json.Serialization;
 
-//Console.WriteLine(BigInteger.Parse(result.ToStringExtension()) + " " + result.ToString());
+TwoSumSolution twoSumSolution = new TwoSumSolution();
+
+Console.WriteLine(JsonConvert.SerializeObject(twoSumSolution.TwoSum([-3, 4, 3, 90], 0)) + " == [0,2]");
--- TwoSumSolution.cs
+++ TwoSumSolution.cs
@@ -8,5 +8,20 @@ namespace LeetCode
 {
     internal class TwoSumSolution
     {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for(int x = 0; x < nums.Length - 1; x++)
+            {
+                for(int y = x + 1; y < nums.Length; y++) 
+                {
+                    if(nums[x] + nums[y] == target)
+                    {
+                        return [ x, y ];
+                    }
+                }
+            }
+           
+            return [];
+        }
     }
 }
