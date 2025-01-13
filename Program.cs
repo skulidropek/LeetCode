d4449ad333cc358098749bfca23a2b0fc4b7e17c
skulidropek <66840575+skulidropek@users.noreply.github.com>
2025-01-13 22:49:17 +0400

    https://leetcode.com/problems/search-a-2d-matrix-ii/

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using LeetCode;
 using Newtonsoft.Json;
+using System.Linq;
 using System.Numerics;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
-UniquePathsSolution solution = new UniquePathsSolution();
+SearchMatrixSolution solution = new SearchMatrixSolution();
 
-Console.WriteLine(solution.UniquePaths(4,5) + " == 35");
+var array = new int[][] { [1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30] };
+int target = 11;
+
+Console.WriteLine(solution.SearchMatrix(array, target) + " == " + Regex.IsMatch(JsonConvert.SerializeObject(array), $"(?<!\\d){target}(?!\\d)"));
---
+++ SearchMatrixSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class SearchMatrixSolution
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            int row = 0, col = matrix[0].Length - 1;
+
+            while (row < matrix.Length && col >= 0)
+            {
+                if (matrix[row][col] == target)
+                    return true;
+
+                if (matrix[row][col] > target)
+                    col--; 
+                else
+                    row++;
+            }
+
+            return false;
+        }
+    }
+}
